088a8b7a95d36e494d4e4456a4e5614e617aa8ed
Brazf <fabiobrazferreira@gmail.com>
2025-04-30 18:04:00 -0300

    feat: add custom logger

--- main.go
+++ main.go
@@ -1,7 +1,25 @@
 package main
 
-import "github.com/Brazf/project-api-go/router"
+import (
+	"github.com/Brazf/project-api-go/config"
+	"github.com/Brazf/project-api-go/router"
+)
+
+var (
+	logger *config.Logger
+)
 
 func main() {
+
+	logger = config.GetLogger("main")
+	//Initializer config
+	err := config.Init()
+
+	//Check config error
+	if err != nil {
+		logger.Errorf("DEU ERRRO: %v", err)
+		return
+	}
+	//Initializer routes
 	router.Initialize()
 }
