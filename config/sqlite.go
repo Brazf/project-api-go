fc311f0bd6d75a86b51bf54c5de23598540cd81a
Brazf <fabiobrazferreira@gmail.com>
2025-05-02 01:25:07 -0300

    feat: add databate create

--- config/sqlite.go
+++ config/sqlite.go
@@ -1,7 +1,43 @@
 package config
 
-import "gorm.io/gorm"
+import (
+	"os"
+
+	"github.com/Brazf/project-api-go/schemas"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+)
 
 func InitializeSQLite() (*gorm.DB, error) {
-
+	logger := GetLogger("SQLite")
+	dbPath := "./db/main.db"
+	// Check if the db file exist
+	_, err := os.Stat(dbPath)
+	if os.IsNotExist(err) {
+		logger.Info("database file not found, creating ...")
+		//creating database file and directory
+		err = os.MkdirAll("./db", os.ModePerm)
+		if err != nil {
+			return nil, err
+		}
+		file, err := os.Create(dbPath)
+		if err != nil {
+			return nil, err
+		}
+		file.Close()
+	}
+	// create DB and connect
+	db, err := gorm.Open(sqlite.Open(dbPath), &gorm.Config{})
+	if err != nil {
+		logger.Errorf("sqlite opening error: %v", err)
+		return nil, err
+	}
+	// Migrate Schema
+	err = db.AutoMigrate(&schemas.Opening{})
+	if err != nil {
+		logger.Errorf("sqlite opening error: %v", err)
+		return nil, err
+	}
+	// Return The DB
+	return db, nil
 }
