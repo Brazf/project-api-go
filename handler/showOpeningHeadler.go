c404d5273c3c981b1379100be5c27c3f9a06b3ca
Brazf <fabiobrazferreira@gmail.com>
2025-06-22 01:12:22 -0300

    feat: update api

--- handler/showOpeningHeadler.go
+++ handler/showOpeningHeadler.go
@@ -20,7 +20,7 @@ func ShowOpeningHandler(ctx *gin.Context) {
 
 	// Find Opening
 	if err := db.First(&opening, id).Error; err != nil {
-		sendError(ctx, http.StatusFound, fmt.Sprintf("opening with id: %s not found", id))
+		sendError(ctx, http.StatusNotFound, fmt.Sprintf("opening with id: %s not found", id))
 		return
 	}
 
